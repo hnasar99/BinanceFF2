9f12410a40463411a308907f74be6f776bd5c994
hnasar99 <hnasar@gmail.com>
2026-09-08 00:19:33 -0300

    Wire character roster into spatial command center

--- app/spatial-next/page.tsx
+++ app/spatial-next/page.tsx
@@ -1,12 +1,14 @@
 import { BabylonCommandCenter } from "./babylon-command-center";
+import { CharacterRoster } from "./character-roster";
 import { CinematicOverlay } from "./cinematic-overlay";
 import "./spatial-next.css";
 
 export default function SpatialNextPage() {
   return (
-    <div style={{ position: "relative", minHeight: "100vh" }}>
+    <div style={{ position: "relative", minHeight: "100vh", overflow: "hidden" }}>
       <BabylonCommandCenter />
       <CinematicOverlay />
+      <CharacterRoster />
     </div>
   );
 }
