a5e8a9ec49c823272cd6bf285036d2b44bc08140
hnasar99 <hnasar@gmail.com>
2026-09-08 00:02:53 -0300

    feat(spatial): layer cinematic HUD over Babylon command center

--- app/spatial-next/page.tsx
+++ app/spatial-next/page.tsx
@@ -1,5 +1,12 @@
 import { BabylonCommandCenter } from "./babylon-command-center";
+import { CinematicOverlay } from "./cinematic-overlay";
+import "./spatial-next.css";
 
 export default function SpatialNextPage() {
-  return <BabylonCommandCenter />;
+  return (
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <BabylonCommandCenter />
+      <CinematicOverlay />
+    </div>
+  );
 }
